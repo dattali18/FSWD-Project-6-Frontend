41fdec70e6f7b5be38de4197dd5034c6fb29e255
Daniel Attali <danielattali16@gmail.com>
2024-07-21 18:44:58 +0300

    added the from in the <Profile />

--- src/pages/profile/profile.jsx
+++ src/pages/profile/profile.jsx
@@ -1,10 +1,104 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../utils/AuthContext";
 
-export default function Profile() {
-   const { user } = useContext(AuthContext);
+import axios from "axios";
 
-    return (<>
-        <h1>Welcome {user.user_name}</h1>
-    </>);
+import { BASE_URL } from "../../data/api";
+
+export default function Profile() {
+  const { user } = useContext(AuthContext);
+  const [userInfo, setUserInfo] = useState({});
+  const [showForm, setShowForm] = useState(false);
+
+  useEffect(() => {
+    // use axios to get user info
+    const url = `${BASE_URL}/api/users/${user.id}`;
+    const getUser = async () => {
+      try {
+        const response = await axios.get(url);
+        const [user_] = response.data.data;
+
+        setUserInfo(user_);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getUser();
+  }, [user.id]);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const url = `${BASE_URL}/api/users/${user.id}`;
+    const putUser = async () => {
+      try {
+        const response = await axios.put(url, userInfo);
+        console.log(response);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    putUser();
+  };
+
+  return (
+    <>
+      <h1>Welcome {user.user_name}</h1>
+      <h2>Profile</h2>
+      <p>Email: {user.email}</p>
+      <button
+        className="btn btn-orange btn-sm"
+        onClick={() => setShowForm(!showForm)}
+      >
+        {showForm ? "Hide" : "Show"} Form
+      </button>
+      {showForm && (
+        <form onSubmit={onSubmit} className="form">
+          <div className="input-group">
+            <label htmlFor="user_name">Username</label>
+            <input
+              type="text"
+              id="user_name"
+              name="user_name"
+              placeholder="username"
+              className="form-input"
+              onChange={(e) =>
+                setUserInfo({ ...userInfo, user_name: e.target.value })
+              }
+              value={userInfo.user_name}
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="first_name">First name</label>
+            <input
+              type="text"
+              id="first_name"
+              name="first_name"
+              placeholder="John"
+              className="form-input"
+              onChange={(e) =>
+                setUserInfo({ ...userInfo, first_name: e.target.value })
+              }
+              value={userInfo.first_name}
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="last_name">Last name</label>
+            <input
+              type="text"
+              id="last_name"
+              name="last_name"
+              placeholder="Doe"
+              className="form-input"
+              onChange={(e) =>
+                setUserInfo({ ...userInfo, last_name: e.target.value })
+              }
+              value={userInfo.last_name}
+            />
+          </div>
+          <button className="btn btn-blue">Add Info</button>
+        </form>
+      )}
+    </>
+  );
 }
--- src/pages/register/register.jsx
+++ src/pages/register/register.jsx
@@ -1,8 +1,41 @@
 import "../style/index.css";
 
+import axios from "axios";
+
+import { BASE_URL } from "../../data/api";
+
+import { useNavigate } from "react-router-dom";
+
 export default function Register() {
+  const navigate = useNavigate();
+
   const onSubmit = (e) => {
     e.preventDefault();
+    const form = e.target;
+    const formData = new FormData(form);
+
+    const email = formData.get("email");
+    const password = formData.get("password");
+    const confirmPassword = formData.get("confirmPassword");
+
+    if (password !== confirmPassword) {
+      alert("Passwords do not match");
+      return;
+    }
+
+    const url = `${BASE_URL}/api/auth/register`;
+
+    const registerUser = async () => {
+      try {
+        const response = await axios.post(url, { email, password });
+        console.log(response);
+        navigate("/login");
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    registerUser();
   };
 
   return (
