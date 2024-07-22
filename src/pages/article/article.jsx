6c582b70890249342758dbd647e159033b713fb4
Daniel Attali <danielattali16@gmail.com>
2024-07-22 12:18:00 +0300

    added color highlighting for the code blocks

--- src/pages/article/article.jsx
+++ src/pages/article/article.jsx
@@ -1,10 +1,11 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-
-import { useState } from "react";
-
 import axios from "axios";
 import { BASE_URL } from "../../data/api";
+
+import Prism from "prismjs";
+import "../style/prism-onedark.css"; // Import the Prism CSS file
+import "prismjs/components/prism-javascript"; // Import the JavaScript language
 
 import "../style/article.css";
 
@@ -15,13 +16,13 @@ export default function Article() {
   // the page is an HTML file
   const [page, setPage] = useState("");
 
-
   useEffect(() => {
     const url = `${BASE_URL}/api/articles/${id}`;
     const fetchArticle = async () => {
       try {
         const response = await axios.get(url);
         setPage(response.data);
+        Prism.highlightAll(); // Highlight code after setting the page content
       } catch (error) {
         console.error("Error fetching article", error);
       }
@@ -29,10 +30,14 @@ export default function Article() {
     fetchArticle();
   }, [id]);
 
+  useEffect(() => {
+    Prism.highlightAll();
+  }, [page]);
+
   return (
     <div>
       <h1>Article</h1>
-      <div className="article" dangerouslySetInnerHTML={{__html: page}}></div>
+      <div className="article" dangerouslySetInnerHTML={{ __html: page }}></div>
     </div>
   );
 }
