18fd3c154fe8d510f02f199bffeb5fcaece0930b
Daniel Attali <danielattali16@gmail.com>
2024-07-22 11:40:32 +0300

    added article rendering in <Article /> page

--- src/pages/article/article.jsx
+++ src/pages/article/article.jsx
@@ -1,15 +1,36 @@
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 
+import { useState } from "react";
+
+import axios from "axios";
+import { BASE_URL } from "../../data/api";
+
 export default function Article() {
-    // get the id of the article from the url
-    const { id } = useParams();
+  // get the id of the article from the url
+  const { id } = useParams();
 
-    // TODO: get the article from the server
+  // the page is an HTML file
+  const [page, setPage] = useState("");
 
-    return (
-        <div>
-            <h1>Article</h1>
-            <p>{id}</p>
-        </div>
-    );
+
+  useEffect(() => {
+    const url = `${BASE_URL}/api/articles/${id}`;
+    const fetchArticle = async () => {
+      try {
+        const response = await axios.get(url);
+        setPage(response.data);
+      } catch (error) {
+        console.error("Error fetching article", error);
+      }
+    };
+    fetchArticle();
+  }, [id]);
+
+  return (
+    <div>
+      <h1>Article</h1>
+      <div dangerouslySetInnerHTML={{__html: page}}></div>
+    </div>
+  );
 }
