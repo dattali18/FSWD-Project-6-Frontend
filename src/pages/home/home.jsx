67f3bcebc28efd0a6cfc2b0390719525cebc1cb3
Daniel Attali <danielattali16@gmail.com>
2024-07-22 11:33:17 +0300

    added style to the <Home /> page

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,9 +1,8 @@
-
-
 export default function Home() {
-    return (<>
-        <h1 className="page-header">
-            Home
-        </h1>
-    </>);
+  return (
+    <>
+      <h2 className="home-header">Human Story & Ideas</h2>
+      <h3 className="home-subheader">A place to read, write, and deepen you understanding</h3>
+    </>
+  );
 }
