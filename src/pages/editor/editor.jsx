593f984ff5b154253f28a9c70ff9e28dcdb2c2f5
Daniel Attali <danielattali16@gmail.com>
2024-08-11 12:53:12 +0300

    added upload box for .md file

--- src/pages/editor/editor.jsx
+++ src/pages/editor/editor.jsx
@@ -11,6 +11,7 @@ import Prism from "prismjs";
 import "prismjs/components/prism-javascript"; // Import the JavaScript language
 import "prismjs/components/prism-python"; // Import the JSX language
 import "prismjs/components/prism-shell-session"; // Import the JSX language
+import "prismjs/components/prism-java"// Import the java
 
 function MyEditor() {
   const [markdown, setMarkdown] = useState("# Hello World");
@@ -32,9 +33,12 @@ function MyEditor() {
     document.body.appendChild(element); // Required for this to work in FireFox
     element.click();
   };
+
+  // adding a drop box to upload a .md file
+
   return (
     <>
-        <h1 className="title">Markdown Editor</h1>
+      <h1 className="title">Markdown Editor</h1>
       <div className="editor">
         <div className="group">
           <textarea
@@ -49,6 +53,25 @@ function MyEditor() {
         <button className="btn btn-blue" onClick={onClick}>
           Download
         </button>
+      </div>
+      <div className="box">
+        <label htmlFor="inputFile" className="file-upload">
+          Custom Upload
+        </label>
+        <input
+          type="file"
+          accept=".md"
+          id="inputFile"
+          className="inputFile"
+          onChange={(e) => {
+            const file = e.target.files[0];
+            const reader = new FileReader();
+            reader.onload = (e) => {
+              setMarkdown(e.target.result);
+            };
+            reader.readAsText(file);
+          }}
+        />
       </div>
     </>
   );
