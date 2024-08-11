a333c93d4bf651b79cf490fc1618a2f3f3e90b4b
Daniel Attali <danielattali16@gmail.com>
2024-08-11 13:07:56 +0300

    added sync scrolling to editor

--- src/pages/editor/editor.jsx
+++ src/pages/editor/editor.jsx
@@ -8,10 +8,10 @@ import "react-markdown-editor-lite/lib/index.css";
 
 import Prism from "prismjs";
 
+import "prismjs/components/prism-java"; // Import the java
 import "prismjs/components/prism-javascript"; // Import the JavaScript language
 import "prismjs/components/prism-python"; // Import the JSX language
 import "prismjs/components/prism-shell-session"; // Import the JSX language
-import "prismjs/components/prism-java"// Import the java
 
 function MyEditor() {
   const [markdown, setMarkdown] = useState("# Hello World");
@@ -34,19 +34,47 @@ function MyEditor() {
     element.click();
   };
 
-  // adding a drop box to upload a .md file
+  // adding synchronized scrolling between the two divs
+  const syncScroll = (e) => {
+    const { target } = e;
+    const { scrollTop, scrollLeft } = target;
+    const other =
+      target.id === "editor-input" ? "editor-output" : "editor-input";
+    const otherElement = document.getElementById(other);
+    otherElement.scrollTop = scrollTop;
+    otherElement.scrollLeft = scrollLeft;
+  };
 
+  // sync the #editor-input and #editor-output scroll
+  // call this block of code at the end of the render
+
+  useEffect(() => {
+    const editorInput = document.getElementById("editor-input");
+    const editorOutput = document.getElementById("editor-output");
+
+    editorInput.addEventListener("scroll", syncScroll);
+    editorOutput.addEventListener("scroll", syncScroll);
+
+    return () => {
+      editorInput.removeEventListener("scroll", syncScroll);
+      editorOutput.removeEventListener("scroll", syncScroll);
+    };
+  });
+
+  // TODO: add a Post button to post the markdown to the server 
+  // make this page protected to writer only
   return (
     <>
       <h1 className="title">Markdown Editor</h1>
       <div className="editor">
         <div className="group">
           <textarea
+            id="editor-input"
             value={markdown}
             onChange={handleEditorChange}
             className="editor-textarea"
           />
-          <div className="html article">
+          <div className="html article" id="editor-output">
             <ReactMarkdown>{markdown}</ReactMarkdown>
           </div>
         </div>
