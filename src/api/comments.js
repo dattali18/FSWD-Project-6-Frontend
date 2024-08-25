ef07cf4e8be7a85bd6466fa09b8bb518ebd31d70
Daniel Attali <danielattali16@gmail.com>
2024-08-25 09:33:10 +0300

    refactored the private comment routes

--- src/api/comments.js
+++ src/api/comments.js
@@ -2,7 +2,7 @@
  * @desc This is the collection of all the routes related to the comments on the articles
  */
 
-import { getToken } from "./../utils/tokenUtil";
+import { authenticatedRequest } from "./../utils/tokenUtil";
 
 import axios from "axios";
 
@@ -10,56 +10,44 @@ import { BASE_URL } from "./../data/api";
 
 const URL = `${BASE_URL}/comments`;
 
-// add the header x-auth-token to the request
-axios.defaults.headers.common["x-auth-token"] = getToken();
-
 /**
  * @desc This function is used to get all the comments on an article
  * @param {Object} comment
  * @param {string} comment.content
  * @param {number} comment.articleId
+ * @access private
  * @returns {Promise}
  */
 async function createComment(comment) {
-  try {
-    return await axios.post(URL, comment);
-  } catch (error) {
-    console.error(error);
-  }
+  return await authenticatedRequest(URL, "post", comment);
 }
 
 /**
  * @desc This function is used to update a comment, only for the author of the comment
  * @param {Object} comment
  * @param {number} commentId
+ * @access private
  * @returns {Promise}
  */
 async function deleteComment(commentId) {
-  try {
-    return await axios.delete(`${URL}/${commentId}`);
-  } catch (error) {
-    console.error(error);
-  }
+  return await authenticatedRequest(`${URL}/${commentId}`, "delete");
 }
 
 /**
  * @desc This function is used to update a comment, only for the author of the comment
  * @param {number} comment_id
  * @param {string} content
+ * @access private
  * @returns {Promise}
  */
 async function updateComment(comment_id, content) {
-  try {
-    const response = await axios.put(`${URL}/`, { comment_id, content });
-    return response;
-  } catch (error) {
-    console.log(error);
-  }
+  return await authenticatedRequest(`${URL}/`, "put", { comment_id, content });
 }
 
 /**
  *  @desc get all the comment on an article
  * @param {number} articleId
+ * @access public
  * @returns {Promise}
  */
 async function getArticleComments(articleId) {
@@ -73,6 +61,7 @@ async function getArticleComments(articleId) {
 /**
  * @desc This function is used to get all the comments of a user
  * @param {number} userId
+ * @access public
  * @returns {Promise}
  */
 async function getUserComments(userId) {
