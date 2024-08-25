9d14c205b4355f5a25717d998ccc168b3debada7
Daniel Attali <danielattali16@gmail.com>
2024-08-25 10:54:38 +0300

    started working on the privileges page

--- src/api/admin.js
+++ src/api/admin.js
@@ -10,6 +10,18 @@ import { BASE_URL } from "./../data/api";
 const URL = `${BASE_URL}/admin`;
 
 
+/**
+ * @desc api call to check if user is admin
+ * @returns {Promise} 
+ */
 export async function isAdmin() {
   return await authenticatedRequest(`${URL}/is-admin`);
+}
+
+/**
+ * @desc api call to get all users
+ * @returns {Promise<[user]>}
+ */
+export async function getUsers() {
+  return await authenticatedRequest(`${URL}/users`);
 }
--- src/pages/admin/privileges/privileges.jsx
+++ src/pages/admin/privileges/privileges.jsx
@@ -1,11 +1,98 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { getUsers } from "../../../api/admin";
+
+import "./privileges.css";
+
+// arrow left and right icons
+import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const PRIVILEGES_PER_PAGE = 5;
+
 export default function Privileges() {
-    /**
-     * this page will have a search bar to search for users
-     * and a list of users with their roles and button to added and remove roles
-     */
+  const [isLoading, setIsLoading] = useState(true);
+  const [users, setUsers] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const totalPages = Math.ceil(users.length / PRIVILEGES_PER_PAGE);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await getUsers();
+        setUsers(response.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const handlePreviousPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const startIndex = (currentPage - 1) * PRIVILEGES_PER_PAGE;
+  const endIndex = startIndex + PRIVILEGES_PER_PAGE;
+  const currentUsers = users.slice(startIndex, endIndex);
+
+  if (isLoading) {
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+
   return (
     <div>
       <h1>Privileges</h1>
+      <div className="users-privileges">
+        <ul className="users">
+          {currentUsers.map((user) => (
+            <li className="user" key={user.id}>
+              <p className="user-username"> {user.username}</p>
+              <Link>{user.role}</Link>
+            </li>
+          ))}
+        </ul>
+        <div className="pagination-controls">
+          <button
+            className={
+              currentPage === 1 ? "btn btn-gray btn-inactive" : "btn btn-blue"
+            }
+            onClick={handlePreviousPage}
+            disabled={currentPage === 1}
+          >
+            <FontAwesomeIcon icon={faArrowLeft} />
+          </button>
+          <span>
+            Page {currentPage} of {totalPages}
+          </span>
+          <button
+            className={
+              currentPage === totalPages
+                ? "btn btn-gray btn-inactive"
+                : "btn btn-blue"
+            }
+            onClick={handleNextPage}
+            disabled={currentPage === totalPages}
+          >
+            <FontAwesomeIcon icon={faArrowRight} />
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
