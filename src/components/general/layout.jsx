4a375cf77b953bbcb992dea61b2eca51433ef8e7
Daniel Attali <danielattali16@gmail.com>
2024-08-25 16:52:40 +0300

    added the message bar component

--- src/components/general/layout.jsx
+++ src/components/general/layout.jsx
@@ -3,12 +3,17 @@ import "./style/index.css";
 import Footer from "./footer.jsx";
 import Navbar from "./navbar.jsx";
 
+import MessageBar from "./messageBar.jsx";
+
 import { Outlet } from "react-router-dom";
 
 export default function Layout() {
   return (
     <>
-      <Navbar />
+      <header>
+        <Navbar />
+        <MessageBar />
+      </header>
       <main>
         <Outlet />
       </main>
---
+++ src/components/general/messageBar.jsx
@@ -0,0 +1,70 @@
+/**
+ * This component is used to display messages to the user.
+ * it will have a message and a close button.
+ * it will have a stack of messages to display.
+ * it will have a timeout to close the message after a certain time.
+ */
+
+// style
+import "./style/messageBar.css";
+
+// icon
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import {
+  faCircleCheck,
+  faCircleExclamation,
+  faCircleInfo,
+  faTriangleExclamation,
+} from "@fortawesome/free-solid-svg-icons";
+
+import PropTypes from "prop-types";
+
+import { useState } from "react";
+
+const categoryIcon = {
+  success: faCircleCheck,
+  alert: faCircleExclamation,
+  warning: faTriangleExclamation,
+  info: faCircleInfo,
+};
+
+export default function MessageBar() {
+  const [messages, setMessages] = useState([
+    { text: "Please take note of something", category: "success" },
+  ]);
+
+  const onClick = () => {
+    console.log("clicked");
+  };
+
+  return (
+    <div className="messages">
+      {messages.map((message, index) => {
+        return <Message key={index} message={message} onClose={onClick} />;
+      })}
+    </div>
+  );
+}
+
+function Message({ message, onClose }) {
+  return (
+    <div className={"message message-" + message.category}>
+      <div className="btn-icon">
+        <FontAwesomeIcon
+          icon={categoryIcon[`${message.category}`] || faCircleInfo}
+        />
+        {message.text}
+      </div>
+      <button className="" onClick={onClose}>
+        <FontAwesomeIcon icon={faTimes} />
+      </button>
+    </div>
+  );
+}
+
+Message.prototype = {
+  message: PropTypes.object.isRequired,
+  onClose: PropTypes.func.isRequired,
+};
